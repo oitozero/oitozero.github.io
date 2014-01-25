d8d4e0784b375b4540640119eeb10833906bc8f4
Pedro Sampaio <pedro@clevertech.biz>
2014-01-25 18:49:46 +0000

    Adding docularing

---
+++ docularing/documentation/docs-metadata.js
@@ -0,0 +1,5 @@
+DOC_DATA=[
+{"section":"section_1","group":"group_1","id":"intro","name":false,"shortName":"Intro To My Code","docType":"overview","module":"intro","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"awesome clear code documented intro is it my naming organized overview to welcome well why","codeBlocks":[
+{"lineStart":1,"lineEnd":9}],"source":false},
+{"section":"section_1","group":"group_1","id":"myModule.moduleSection:thisIsAFunction","name":false,"shortName":"thisIsAFunction","docType":"function","module":"myModule","moduleSection":"moduleSection","moduleItem":"thisIsAFunction","moduleSubItem":"","keywords":"description function is modulesection mymodule of part rules still the this","codeBlocks":[
+{"lineStart":3,"lineEnd":9}],"source":false}];
---
+++ docularing/documentation/groups-metadata.js
@@ -0,0 +1,3 @@
+GROUP_DATA=[
+{"groupId":"group_1","groupTitle":"Group 1","groupIcon":"icon-book","showSource":false,"sections":[
+{"id":"section_1","title":"Section 1","showSource":false,"scripts":["lib/index.js"],"docs":["doc/lib.doc"],"doc_api":"doc"}],"visible":true}];
